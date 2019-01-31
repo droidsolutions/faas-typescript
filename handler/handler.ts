40384a225660b3c40d93a48eb6d78e9b0907e97a
Mathias Vonende <mathias.vonende@droidsolutions.de>
2019-01-31 16:43:37 +0100

    chore: default handler as class

--- handler/handler.ts
+++ handler/handler.ts
@@ -1,21 +1,28 @@
-import * as fs from "fs";
-import {buffer} from "get-stdin";
+import { buffer } from "get-stdin";
 import * as os from "os";
 
-const contentValid = (content: string) => {
-  return content && content.length && content.indexOf("verbose") > -1;
-};
+class Handler {
+  constructor() {}
 
-buffer().then((content: any) => {
-  fs.readFile("/etc/hostname", "utf8", (_, data: any) => {
-    console.log("Hostname: " + data);
-    console.log("Platform: " + os.platform());
-    console.log("Arch: " + os.arch());
-    console.log("CPU count: " + os.cpus().length);
-    console.log("Uptime: " + os.uptime());
-    if (contentValid(content)) {
-      console.log(os.cpus());
-      console.log(os.networkInterfaces());
+  public initialize(): void {
+    buffer().then((content: any) => {
+      this.handler(content);
+    });
+  }
+
+  private handler(stdin: any): void {
+    if (this.contentValid(stdin)) {
+      console.log("Platform: " + os.platform());
+      console.log("Arch: " + os.arch());
+      console.log("CPU count: " + os.cpus().length);
+      console.log("Uptime: " + os.uptime());
     }
-  });
-});
+  }
+
+  private contentValid(content: string): boolean {
+    return typeof content === "string" && content.length > 0 && content.indexOf("verbose") > -1;
+  }
+}
+
+const handler = new Handler();
+handler.initialize();
